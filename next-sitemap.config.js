fb639f1df79602b7e11f21d2c2d5878437aae1ca
Denys <dkss@ukr.net>
2026-05-11 19:21:43 +0300

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,1 +1,15 @@
-
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://beton.monster', // Ваш основной домен
+  generateRobotsTxt: true, // Создать заодно и robots.txt
+  sitemapSize: 7000,
+  exclude: ['/server-sitemap.xml'], // Если будут динамические страницы позже
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
+}
